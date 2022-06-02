e76595a6b719ddff7900edc7e193e9c0758e8d9e
Theophilus <theophilusben2002@gmail.com>
2022-06-02 13:39:34 -0700

    comming this commands

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,8 +1,11 @@
 #include <stdio.h>
-
-int main()
+/** betty code style */
+/*
+ * this is the main fuction where all our codes will be called
+ * befor being executed
+ */
+int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
-	
 	return (0);
 }
