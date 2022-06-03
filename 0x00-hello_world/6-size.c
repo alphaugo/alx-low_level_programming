d212773a4a1cca8204cde68c96020200e0617e2b
Theophilus <theophilusben2002@gmail.com>
2022-06-02 21:13:06 -0700

    fixed the trailing whitespace issue

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-/** 
+/**
  * main - Entry point
  * Return: Always 0 (success)
  */
@@ -11,4 +11,4 @@ int main(void)
 	printf("Size of a long long int: %ld byte(s)\n", sizeof(long long int));
 	printf("size of a float: %ld byte(s)\n", sizeof(float));
 	return (0);
-}  
+}
