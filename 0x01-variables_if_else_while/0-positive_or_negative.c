0d701fba39bced28b65f0465924f4fc4dab783c4
Theophilus <theophilusben2002@gmail.com>
2022-06-03 12:52:42 -0700

    commiting this command

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	/* my code */
+	if (n > 0)
+		printf(" %d is positve \n", n);
+	else
+		printf(" %d is negative\n", n);
+}
