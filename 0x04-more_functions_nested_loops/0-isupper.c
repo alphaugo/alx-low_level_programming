a27243215e7d53dd6ed7fa4b7906adf004a8b7bf
Theophilus <theophilusben2002@gmail.com>
2022-06-09 13:40:41 -0700

    task 0

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * main - check the code.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	char c;
+
+	if (c == 'A')
+		_putchar('1');
+	else
+		_putchar('0');
+
+	return (0);
+}
