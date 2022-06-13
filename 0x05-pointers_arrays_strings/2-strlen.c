c62c3423fe5ef46ebf1fe5b82a200ae7e764aef1
Theophilus <theophilusben2002@gmail.com>
2022-06-13 09:04:39 -0700

    commiting task 2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * _strlen - function that returns the length of string
+ *
+ * @s: a pointer to a character
+ *
+ * Return: int
+ */
+int _strlen(char *s)
+{
+	strlen(*s);
+}
