cd703ed47187650028410c472ece5dc25b207c32
Theophilus <theophilusben2002@gmail.com>
2022-06-13 08:13:12 -0700

    commiting task 0

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -8,7 +8,7 @@
 void reset_to_98(int *n)
 {
 	int a;
-	a = &n;
-	
+	n = &a;
+
 	*n = 98;
 }
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,9 +1,9 @@
-#ifndef "MAIN_H"
-#define "MAIN_H"
+#ifndef MAIN_H
+#define MAIN_H
 
-#include <stdio.h>
 
 void reset_to_98(int *n);
 int _putchar(char c);
+
 #endif
 
