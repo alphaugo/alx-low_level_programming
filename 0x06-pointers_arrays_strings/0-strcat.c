24e4105af04265dc696f1c6ac804c41c2e52eac0
Theophilus <theophilusben2002@gmail.com>
2022-06-15 12:42:28 -0700

    commiting task 0

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * _strcar - function or pointer function
+ * _strcat - function or pointer function
  *
  * @dest: a character pointer
  * @src: a character pointer
@@ -12,19 +12,15 @@
 
 char *_strcat(char *dest, char *src)
 {
-    while(*dest != '\0')
-    {
-        if(*dest != '\0')
-            printf("%c", *dest);
-        dest++;
-    
-    }
-    while (*src != '\0')
-     {
-        if(*src != '\0')
-            printf("%c", *src);
-        src++;
-     }
-    printf("\n");
-     return 0;
+	while (*dest != '\0')
+	{
+		printf("%c", *dest);
+		dest++;
+	}
+	while (*src != '\0')
+	{
+		printf("%c", *src);
+		src++;
+	}
+	printf("\n");
 }
