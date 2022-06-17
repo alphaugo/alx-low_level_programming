1b4956b0c692dce1dc77f737f0c1694d2faa4b13
Theophilus <theophilusben2002@gmail.com>
2022-06-17 01:34:48 -0700

    commiting this task

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,3 +1,14 @@
+#include "main.h"
+
+/**
+ * _strncat - function that concates two strings.
+ *
+ * @dest: pointer to destination char
+ * @src: pointer to source char.
+ * @n: number of bytes to receive
+ * Return: char
+ */
+
 char *_strncat(char *dest, char *src, int n)
 {
 	unsigned int i = 0;
--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,30 +1,31 @@
 #include "main.h"
+
 /**
- * _strcmp - fumction that compares two strings
+ * _strcmp - function that compares two strings.
  *
- * @s1: a character pointer
- * @s2: a character pointer
- *
- * Retrun: int
+ * @s1: pointer to char source 1
+ * @s2: pointer to char source 2
+ * Return: int
  */
-int _strcmp(char *s1; char *s2)
+
+int _strcmp(char *s1, char *s2)
 {
 	int i = 0;
-	int amb;
+	int aux;
 
 	while (*(s1 + i) != '\0')
 	{
 		if (*(s1 + i) > *(s2 + i))
 		{
-			amb = *(s1 + i) - *(s2 + i);
-			return (amb);
+			aux = *(s1 + i) - *(s2 + i);
+			return (aux);
 		}
 		else if (*(s1 + i) < *(s2 + i))
 		{
-			amb = *(s1 + i) - *(s2 + i);
-			return (amb);
+			aux = *(s1 + i) - *(s2 + i);
+			return (aux);
 		}
-		i++;
+		i += 1;
 	}
 	return (0);
 }
