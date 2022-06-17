862d8b586e36c3eb6b48727702e3ffe6ed5d03a7
Theophilus <theophilusben2002@gmail.com>
2022-06-17 01:06:15 -0700

    commiting this task

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ * _strcmp - fumction that compares two strings
+ *
+ * @s1: a character pointer
+ * @s2: a character pointer
+ *
+ * Retrun: int
+ */
+int _strcmp(char *s1; char *s2)
+{
+	int i = 0;
+	int amb;
+
+	while (*(s1 + i) != '\0')
+	{
+		if (*(s1 + i) > *(s2 + i))
+		{
+			amb = *(s1 + i) - *(s2 + i);
+			return (amb);
+		}
+		else if(*(s1 + i) < *(s2 + i))
+		{
+			amb = *(s1 + i) - *(s2 + i);
+			return (amb);
+		}
+		i++;
+	}
+	return (0);
+}
