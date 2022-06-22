cad985a15b1cbb391b1328e57679932b99be961a
Theophilus <theophilusben2002@gmail.com>
2022-06-22 13:47:53 -0700

    commiting task 0

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -8,6 +8,13 @@
  */
 void _puts_recursion(char *s)
 {
-	if ((*s) != '0')
-		return _puts_recursion(s);
+	if (*s != '\0')
+	{
+		_putchar(s);
+		return _puts_recursion(s + 1);
+	}
+	else
+	{
+		_putchar('\n');
+	}
 }
