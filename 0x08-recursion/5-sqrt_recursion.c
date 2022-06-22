3f39268c648975703ef0e5599ae3107087e7cd4b
Theophilus <theophilusben2002@gmail.com>
2022-06-22 14:23:11 -0700

    commiting this task

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -7,7 +7,7 @@
  */
 int _sqrt_recursion(int n)
 {
-	return (_sqrt(n, 1));
+	return (_sqrt_recursion(n, 1));
 }
 
 /**
@@ -16,7 +16,7 @@ int _sqrt_recursion(int n)
  * @i: integer parameter
  * Return: sqrt
  */
-int _sqrt(int n, int i)
+int _sqrt_recursion(int n, int i)
 {
 	if (n < 0)
 		return (-1);
