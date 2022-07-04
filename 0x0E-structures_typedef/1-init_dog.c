a70de68eb0d2053907c70e594c1e944278387164
Theophilus <theophilusben2002@gmail.com>
2022-07-04 11:10:22 -0700

    commiting this command

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,24 @@
+#include "dog.h"
+/**
+ * init_dog - fuction that takes in some parameters
+ * @dog: a pointer to the structure dog
+ * @name: a parameter that takes in the name of the dog
+ * @age: takes in the age of the dog
+ * @owner: holds the name of the owner
+ * @d: points to the structure dog
+ *
+ * Description: fuction that take parameter
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	if (d == NULL)
+		return;
+
+
+	else
+	{
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
+	}
+}
