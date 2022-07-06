909384d89b7d568637f3acf6e296aeedbdea9f62
Theophilus <theophilusben2002@gmail.com>
2022-07-06 11:07:41 -0700

    commiting this command

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,12 @@
+#include "function_pointers.h"
+/**
+ * print_name - function that prints char passed
+ * @name: character to print
+ * @f: pointer to a function
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	if (name != NULL && f != NULL)
+		f(name);
+}
---
+++ 0x0F-function_pointers/function_pointers.h
@@ -0,0 +1,6 @@
+#ifndef _FUNCTIONS_
+#define _FRNCTIONS_
+
+void print_name(char *name, void (*f)(char *));
+
+#endif
