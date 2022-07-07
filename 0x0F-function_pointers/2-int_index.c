39bddb34b860bcbd96a20bf5cf0adc0aaffb13d9
Theophilus <theophilusben2002@gmail.com>
2022-07-07 03:47:05 -0700

    commiting this command

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,29 @@
+#include "function_pointers.h"
+#include <stddef.h>
+/**
+ * int_index - function
+ * @array: array 
+ * @size: size of array
+ * @cmp: a fumction pointer
+ *
+ * Returne: int index
+ */
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i = 0;
+
+	if (size > 0)
+	{
+		if (array != NULL && cmp != NULL)
+		{
+			while (i < size)
+			{
+				if (cmp(array[i])
+					return (i);
+				i++;
+			}
+		}
+	}
+}
+
