eedebdab5fd5a0bb3a2bc471913b342739f17a84
Theophilus <theophilusben2002@gmail.com>
2022-09-02 13:41:44 -0700

    task 2

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -12,15 +12,25 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 	// allpcate node
 	dlistint_t *new_node = malloc(sizeof(dlistint_t));
 
+	if (new_node == NULL)
+		return (NULL);
+
 	// put in the data
 	new_node->n = n;
+	if (*head)
+	{	
+		// make next of new node ad head
+		new_node->next = (*head);
+		new_node->prev = (*head)->prev;
+		(*head)->prev = new_node;
 
-	// make next of new node ad head
-	new_node->next = (*head);
-
-	// move the head to point at the new node
-	(*head) = new_node;
-	if (new_node)
+		// move the head to point at the new node
+		(*head) = new_node;
 		return (*head);
-	return (NULL);
+	}
+
+	new_node->next = *head;
+	new_node->prev = NULL;
+	*head = new_node;
+	return (*head);
 }
