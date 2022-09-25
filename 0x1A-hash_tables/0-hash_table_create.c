2d07615f71b4b3aebfec6784a655345706b11a32
Theophilus <theophilusben2002@gmail.com>
2022-09-25 10:59:10 -0700

    commiting task

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -1,17 +1,30 @@
 #include "hash.h"
+
 /**
- * hash_table_create - fumction
- * @size: ...
- * Return: pointer
- */
+  * hash_table_create - Creates a hash table
+  * @size: Size of the array
+  *
+  * Return: A pointer to the newly created hash table
+  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
-	hash_table_t *table = malloc(sizeof(hash_table_t));
-	table->size = size;
-	table->array = calloc (table->size, sizeof(hash_node_t));
+	unsigned long int i = 0;
+	hash_table_t *htable = NULL;
+	hash_node_t **arr = NULL;
 
-	for (int i = 0; i < table->size; i++)
-		table->array[i] = NULL;
+	htable = malloc(sizeof(hash_table_t));
+	if (htable == NULL)
+		return (NULL);
 
-	return table;
+	arr = malloc(sizeof(hash_node_t *) * size);
+	if (arr == NULL)
+		return (NULL);
+
+	for (; i < size; ++i)
+		arr[i] = NULL;
+
+	htable->size = size;
+	htable->array = arr;
+
+	return (htable);
 }
